4fde2b62aab4f25f6186880186f8b838f1d78e7b
Erick <erickd.cadena@gmail.com>
2025-05-06 09:55:17 -0500

    versio1

---
+++ src/main/java/HolaServlet.java
@@ -0,0 +1,17 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet ("HolaServlet")
+public class HolaServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
